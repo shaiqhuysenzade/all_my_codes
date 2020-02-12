3dff3d295a60409c8af6115b687531b345042d46
Shaiq Huseynzade <shaiqhuseynzade98@gmail.com>
2020-02-13 00:34:01 +0400

    --simple homework2-- --simple and other advanced tasks for homework1--

--- src/main/java/homeworks/Homework1.java
+++ src/main/java/homeworks/Homework1.java
@@ -1,13 +1,17 @@
 package homeworks;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Homework1 {
     public static void main(String[] args) {
         Random rn = new Random();
+//        ArrayList<Integer> users_numbers = new ArrayList();
         //random number choosing by system
         int random_number = rn.nextInt(100);
+        int count=0;
         System.out.println(random_number);
         System.out.println("Let the game begin!");
         Scanner in = new Scanner(System.in);
@@ -15,17 +19,26 @@ public class Homework1 {
         System.out.println("Enter your name,please: ");
         String name = in.nextLine();
         //create loop
-        for (int i = 0; ; i++) {
+        while (true) {
             System.out.println("Enter your number ");
             int number = in.nextInt();
+//            users_numbers.add(number);
             if (number == random_number) {
+                count++;
                 System.out.println("Congratulations, " + name);
                 break;
             } else if (number < random_number) {
+                count++;
                 System.out.println("Your number is too small. Please, try again.");
             } else if (number > random_number) {
                 System.out.println("Your number is too big. Please, try again.");
+                count++;
             }
         }
+        System.out.println(count);
+//        Collections.reverse(users_numbers);
+//        for (int i : users_numbers) {
+//            System.out.print(i + " ");
+//        }
     }
 }
