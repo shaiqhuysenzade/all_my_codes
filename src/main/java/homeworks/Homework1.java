fe67dce4fe5ba29c7e8a721d6e4f67e5ebca6803
Shaiq Huseynzade <shaiqhuseynzade98@gmail.com>
2020-02-16 14:45:09 +0400

    --homework1 modifyed it --

--- src/main/java/homeworks/Homework1.java
+++ src/main/java/homeworks/Homework1.java
@@ -1,44 +1,58 @@
 package homeworks;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Homework1 {
     public static void main(String[] args) {
         Random rn = new Random();
-        ArrayList<Integer> users_numbers = new ArrayList();
         //random number choosing by system
         int random_number = rn.nextInt(100);
-        int count=0;
-//        System.out.println(random_number);
+        int count = 0;
+        System.out.println(random_number);
         System.out.println("Let the game begin!");
         Scanner in = new Scanner(System.in);
         //name entry
         System.out.println("Enter your name,please: ");
         String name = in.nextLine();
+        int[] arrayOfNumbers = new int[1000];
         //create loop
+        int i = 0;
         while (true) {
             System.out.println("Enter your number ");
             int number = in.nextInt();
-            users_numbers.add(number);
+            count++;
             if (number == random_number) {
-                count++;
+                arrayOfNumbers[i] = number;
+                i++;
                 System.out.println("Congratulations, " + name);
                 break;
             } else if (number < random_number) {
-                count++;
+                arrayOfNumbers[i] = number;
+                i++;
                 System.out.println("Your number is too small. Please, try again.");
             } else if (number > random_number) {
+                arrayOfNumbers[i] = number;
+                i++;
                 System.out.println("Your number is too big. Please, try again.");
-                count++;
             }
         }
-        System.out.println(count);
-        Collections.reverse(users_numbers);
-        for (int i : users_numbers) {
-            System.out.print("Your numbers are : "+i + " ");
+        System.out.println("Your input " + count + " numbers ");
+        System.out.print("The numbers which you input is :  ");
+        for (i = 0; i < arrayOfNumbers.length; i++) {
+            int t = 0;
+            for (int j = i + 1; j < arrayOfNumbers.length; j++) {
+                if (arrayOfNumbers[i] < arrayOfNumbers[j]) {
+                    t = arrayOfNumbers[i];
+                    arrayOfNumbers[i] = arrayOfNumbers[j];
+                    arrayOfNumbers[j] = t;
+                }
+            }
+        }
+        //output in reverse order
+        for (i = 0; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] != 0)
+                System.out.print(arrayOfNumbers[i] + " ");
         }
     }
 }
+
